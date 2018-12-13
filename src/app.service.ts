56f3d8c3e21e27f1d982c6f582920737de0eb67b
Paths <6thPath>
2018-12-13 17:04:15 +0700

    x

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
   root(): string {
-    return 'Hello World!'
+    return `
+      Hello World!
+      abcs
+      `
   }
 }
