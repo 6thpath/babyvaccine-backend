4e1271990add834dc531d0dd993b64f9dc6581c9
Paths <6thPath>
2018-12-13 14:57:30 +0700

    hotfix port error

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import * as env from 'dotenv'
+env.config()
 import { NestFactory, FastifyAdapter } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter())
+  console.log(process.env.PORT)
   await app.listen(process.env.PORT || 3000)
 }
 
