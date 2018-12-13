4df7df4546f1c254f7c303edd4811610162c3cce
Paths <6thPath>
2018-12-13 15:25:09 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter())
   console.log(process.env.PORT)
-  await app.listen(process.env.PORT || 3000)
+  await app.listen(process.env.PORT || 3000, '0.0.0.0')
 }
 
 bootstrap()
