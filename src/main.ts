89e7e8a412d6e843f7cb1348a1cd69003800d8ea
Phat Mai <tienphat1501@gmail.com>
2018-12-17 16:24:20 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import * as env from 'dotenv'
 env.config()
 import { NestFactory, FastifyAdapter } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter())
+  app.use(cors())
   await app.listen(process.env.PORT || 3000, '0.0.0.0')
 }
 
